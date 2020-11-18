cd898c371577556f6daa69c02e97d74b982273a8
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-11-18 20:05:02 +0100

    :art: Added partials to application

--- loaders/express.loader.js
+++ loaders/express.loader.js
@@ -2,10 +2,11 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const hbs = require("hbs");
 
 const app = express();
 
-// view engine setup
+hbs.registerPartials(path.join(__dirname, "../views/partials"));
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "hbs");
 
@@ -13,6 +14,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 module.exports = app;
