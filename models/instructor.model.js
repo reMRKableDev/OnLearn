6c073a992ab664d68b7c3f58d0cdb6082da0990d
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-11-18 19:39:26 +0100

    :card_file_box: Added new models to application

---
+++ models/instructor.model.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require("mongoose");
+
+const instructorSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  username: String,
+  email: String,
+  classes: [
+    {
+      class_id: { type: [mongoose.Schema.Types.ObjectId] },
+      class_title: { type: String },
+    },
+  ],
+});
+
+module.exports = model("Instructor", instructorSchema);
---
+++ models/user.model.js
@@ -0,0 +1,22 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: [true, "Username is required."],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required."],
+    match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: { type: String, required: [true, "Password is required."] },
+  role: { type: String },
+});
+
+module.exports = model("User", userSchema);
