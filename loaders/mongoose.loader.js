07ca2a1b35a1b4d3f16b70267bb62218ef3147ea
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-11-18 19:28:29 +0100

    :heavy_plus_sign: Added mongoose dependency + loader

---
+++ loaders/mongoose.loader.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { localMongoUri } = require("../configs");
+
+module.exports = async () => {
+  try {
+    const db = await mongoose.connect(localMongoUri, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      useFindAndModify: true,
+    });
+    console.log(
+      `Connected to Mongo! Database name: "${db.connections[0].name}"`
+    );
+  } catch (err) {
+    console.error(`Error connecting to mongo: ${err}`);
+  }
+};
