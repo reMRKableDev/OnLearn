ccafe83e0373e6fc2fce5ff3cf3b7cb902be752c
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-12-06 20:11:48 +0100

    :white_check_mark: tests for configuration folder

---
+++ configs/__tests__/index.test.js
@@ -0,0 +1,8 @@
+const configsObject = require('../index');
+const { validateNotEmpty } = require('../../utils/validators.utils');
+
+describe('Configuration Test Suite', () => {
+  test('should validate configuration object', () => {
+    validateNotEmpty(configsObject);
+  });
+});
