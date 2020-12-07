d3b2e5c38b77cbe3c020ee259851a0a0a8e499ac
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-12-07 19:33:49 +0100

    :recycle: brought back refactored changes for CI build

--- models/fixtures/index.js
+++ models/fixtures/index.js
@@ -1,37 +1,29 @@
-const {
-  dummyRole,
-  dummyData,
-  dummyEmail,
-  dummyPassword,
-  dummyUsername,
-} = require('../../configs');
-
 exports.fakeClassDataNoLessons = {
-  title: dummyData,
-  description: dummyData,
-  instructor: dummyData,
-  imageUrl: dummyData,
+  title: 'Dummy Class',
+  description: 'Dummy Description',
+  instructor: 'Dummy Instructor',
+  imageUrl: 'http://dummy.image.com',
   lessons: [],
 };
 
 exports.fakeUserDataNoClasses = {
-  firstName: dummyData,
-  lastName: dummyData,
-  username: dummyUsername,
-  email: dummyEmail,
+  firstName: 'Dummy',
+  lastName: 'User',
+  username: 'dummyUser',
+  email: 'dummy@user.com',
   classes: [],
 };
 
 exports.fakeUserData = {
-  username: dummyUsername,
-  email: dummyEmail,
-  password: dummyPassword,
-  role: dummyRole,
+  username: 'studentUser',
+  email: 'student@student.com',
+  password: '********',
+  role: 'student',
 };
 
 exports.fakeUserDataEmptyFields = {
   username: '',
   email: '',
   password: '',
-  role: dummyRole,
+  role: 'student',
 };
