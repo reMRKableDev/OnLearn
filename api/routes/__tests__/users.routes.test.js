819e45dbf2885433736122219b5040647e30c1bd
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-12-15 09:37:54 +0100

    :white_check_mark: Fixed breaking User auth routes test

--- api/routes/__tests__/users.routes.test.js
+++ api/routes/__tests__/users.routes.test.js
@@ -1,4 +1,4 @@
-const usersRouter = require('../users.routes');
+const usersAuthRouter = require('../users-auth.routes');
 const {
   createNewUserController,
   renderRegisterController,
@@ -12,20 +12,20 @@ const {
 
 describe('Users Router Test Suite', () => {
   test('should validate GET route handler', () => {
-    const { path, stack } = usersRouter.stack[0].route;
+    const { path, stack } = usersAuthRouter.stack[0].route;
     const { handle, method } = stack[0];
 
-    validateNotEmpty(usersRouter.stack[0].route);
+    validateNotEmpty(usersAuthRouter.stack[0].route);
     validateStringEquality(path, '/register');
     validateStringEquality(method, 'get');
     validateControllerUsed(handle, renderRegisterController);
   });
 
   test('should validate POST route handler', () => {
-    const { path, stack } = usersRouter.stack[1].route;
+    const { path, stack } = usersAuthRouter.stack[1].route;
     const { handle, method } = stack[stack.length - 1];
 
-    validateNotEmpty(usersRouter.stack[1].route);
+    validateNotEmpty(usersAuthRouter.stack[1].route);
     validateStringEquality(path, '/register');
     validateStringEquality(method, 'post');
     validateStringEquality(handle, createNewUserController);
