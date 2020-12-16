39fdc9ed8391aa44926082ecb1b93f5a3207eef7
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-12-16 16:16:18 +0100

    :white_check_mark: Coverage fixed

---
+++ api/controllers/usersControllers/__tests__/logoutUser.controller.test.js
@@ -0,0 +1,27 @@
+const { logoutUserController } = require('../index');
+const {
+  mockRequest,
+  mockResponse,
+} = require('../../../../utils/test-utils/interceptors.utils');
+const {
+  validateMockValueToHaveBeenCalled,
+} = require('../../../../utils/test-utils/validators.utils');
+
+describe('logoutUser Controller Test Suite', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('should validate logout path', () => {
+    const req = mockRequest();
+    const res = mockResponse();
+
+    const { redirect } = res;
+    const { logout } = req;
+
+    logoutUserController(req, res);
+
+    validateMockValueToHaveBeenCalled(logout);
+    validateMockValueToHaveBeenCalled(redirect);
+  });
+});
