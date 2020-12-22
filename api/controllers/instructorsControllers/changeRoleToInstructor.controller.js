6f4132bc1bbffa3c5e1064ccab5a430dbd7f9617
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-12-22 16:58:26 +0100

    :art: Introduced 500 helper to controller

--- api/controllers/instructorsControllers/changeRoleToInstructor.controller.js
+++ api/controllers/instructorsControllers/changeRoleToInstructor.controller.js
@@ -1,20 +1,14 @@
 const {
   updateUserRoleService,
 } = require('../../../database/services/modelServices/userServices');
+const { render500ErrorHelper } = require('../helpers');
 
 exports.changeRoleToInstructor = async (req, res) => {
   const { _id: userId } = req.user;
   const updatedResults = await updateUserRoleService(userId);
 
   if (updatedResults instanceof Error) {
-    res.status(500).render('error', {
-      message:
-        'Oops! An unexpected error seems to have occurred while processing your request.',
-      error: {
-        status: '500',
-        stack: `We're sorry for the trouble. We've been notified and will correct this as soon as possible. Please try your request again after a little while`,
-      },
-    });
+    render500ErrorHelper(res);
     return;
   }
 
