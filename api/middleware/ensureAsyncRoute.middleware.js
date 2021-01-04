d37982f16b681333fb510f7ec5ef17944a2fa2d3
Malcolm Kente <malcolm.kente@remrkabledev.com>
2021-01-04 18:32:44 +0100

    :fire: Deleted unused middlewar

--- api/middleware/ensureAsyncRoute.middleware.js
+++
@@ -1,2 +0,0 @@
-exports.ensureAsyncRoutes = (route) => (req, res, next = console.error) =>
-  Promise.resolve(route(req, res)).catch(next);
--- api/middleware/index.js
+++ api/middleware/index.js
@@ -1,6 +1,5 @@
 const { isInstructor } = require('./isInstructor.middleware');
 const { isLoggedInUser } = require('./isLoggedInUser.middleware');
-const { ensureAsyncRoutes } = require('./ensureAsyncRoute.middleware');
 const {
   validateRegistrationForm,
 } = require('./validateRegistrationForm.middleware');
@@ -8,6 +7,5 @@ const {
 module.exports = {
   isInstructor,
   isLoggedInUser,
-  ensureAsyncRoutes,
   validateRegistrationForm,
 };
