33f2a34c46a33e663975c88237eb2716e386b902
Malcolm Kente <malcolm.kente@remrkabledev.com>
2021-01-04 18:11:51 +0100

    :white_check_mark: New test case for rendering landing page

--- api/controllers/indexControllers/__tests__/renderIndexView.controller.test.js
+++ api/controllers/indexControllers/__tests__/renderIndexView.controller.test.js
@@ -7,14 +7,31 @@ const {
   mockResponse,
 } = require('../../../../utils/test-utils/interceptors.utils');
 
+let req;
+let res;
+
 describe('renderIndexView Test Suite', () => {
+  beforeEach(() => {
+    req = mockRequest();
+    res = mockResponse();
+  });
+
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  test('should validate res.status & res.render is called', () => {
-    const req = mockRequest();
-    const res = mockResponse();
+  test('should validate res.status & res.render is called when a user is logged in', () => {
+    req.user = { local: { username: expect.anything() } };
+
+    renderIndexViewController(req, res);
+    const { status, render } = res;
+
+    validateMockValueToHaveBeenCalled(status);
+    validateMockValueToHaveBeenCalled(render);
+  });
+
+  test('should validate res.status & res.render is called when there is no user logged in', () => {
+    req.user = null;
 
     renderIndexViewController(req, res);
     const { status, render } = res;
