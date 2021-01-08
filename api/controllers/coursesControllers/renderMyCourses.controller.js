ce0edfdf8a7faa9149fcc5d5eaad33a80f1dd6bb
Malcolm Kente <malcolm.kente@remrkabledev.com>
2021-01-08 11:39:09 +0100

    :art: Removed console.log and unneeded test folder

--- api/controllers/coursesControllers/renderMyCourses.controller.js
+++ api/controllers/coursesControllers/renderMyCourses.controller.js
@@ -20,8 +20,6 @@ exports.renderMyCoursesController = async (req, res) => {
 
   const user = { role, coursesTaught, coursesLearned };
 
-  console.log(user);
-
   res.status(200).render('users/common/my-courses', {
     user,
     local,
