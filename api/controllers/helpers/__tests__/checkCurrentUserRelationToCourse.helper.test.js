67b16488da26f6553d15dc3945c8d557d14a3c5d
Malcolm Kente <malcolm.kente@remrkabledev.com>
2021-01-10 01:09:44 +0100

    :white_check_mark: check user relation

---
+++ api/controllers/helpers/__tests__/checkCurrentUserRelationToCourse.helper.test.js
@@ -0,0 +1,64 @@
+const { checkCurrentUserRelationToCourseHelper } = require('../index');
+
+const {
+  validateEquality,
+} = require('../../../../utils/test-utils/validators.utils');
+
+let dummyInstructorsList;
+let dummyStudentsList;
+
+const dummyInstructorOneId = 'fedfe6ed';
+const dummyInstructorTwoId = 'f7e52938';
+
+const dummyStudentOneId = '94aa6839';
+const dummyStudentTwoId = 'd30320f3';
+const dummyStudentThreeId = '9c622219';
+
+describe('checkCurrentUserRelationToCourse Helper Test Suite', () => {
+  beforeEach(() => {
+    dummyInstructorsList = [
+      { _id: dummyInstructorOneId },
+      { _id: dummyInstructorTwoId },
+    ];
+    dummyStudentsList = [
+      { _id: dummyStudentOneId },
+      { _id: dummyStudentTwoId },
+      { _id: dummyStudentThreeId },
+    ];
+  });
+
+  test('should validate current user as an instructor', () => {
+    const dummyCurrentUserId = dummyInstructorTwoId;
+
+    const {
+      isCurrentUserInStudentList,
+      isCurrentUserTheCourseInstructor,
+    } = checkCurrentUserRelationToCourseHelper(
+      dummyInstructorsList,
+      dummyCurrentUserId,
+      dummyStudentsList
+    );
+
+    console.log('student', isCurrentUserInStudentList);
+    console.log('instructor', isCurrentUserTheCourseInstructor);
+
+    validateEquality(isCurrentUserInStudentList, false);
+    validateEquality(isCurrentUserTheCourseInstructor, true);
+  });
+
+  test('should validate current user as a student', () => {
+    const dummyCurrentUserId = dummyStudentThreeId;
+
+    const {
+      isCurrentUserInStudentList,
+      isCurrentUserTheCourseInstructor,
+    } = checkCurrentUserRelationToCourseHelper(
+      dummyInstructorsList,
+      dummyCurrentUserId,
+      dummyStudentsList
+    );
+
+    validateEquality(isCurrentUserInStudentList, true);
+    validateEquality(isCurrentUserTheCourseInstructor, false);
+  });
+});
