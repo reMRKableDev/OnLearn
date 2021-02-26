ccbf715212c53c33a51f78351b45135cf1bf0095
Malcolm Kente <malcolm.kente@remrkabledev.com>
2021-02-26 19:43:41 +0100

    :white_check_mark: New tests added for rendering lesson details

---
+++ api/controllers/lessonsControllers/__tests__/renderLessonDetails.controller.test.js
@@ -0,0 +1,63 @@
+const { renderLessonDetailsController } = require('../index');
+const {
+  setupReqRes,
+  clearMocks,
+} = require('../../../../utils/test-utils/courseControllerDeps');
+const {
+  findOneLessonService,
+} = require('../../../../database/services/modelServices/lessonServices');
+const {
+  render500ErrorHelper,
+  redirectNonexistentCourseHelper,
+} = require('../../helpers');
+const {
+  validateMockValueToHaveBeenCalled,
+} = require('../../../../utils/test-utils/validators.utils');
+
+jest.mock('../../helpers');
+jest.mock('../../../../database/services/modelServices/lessonServices');
+
+let req;
+let res;
+
+describe('renderLessonDetails Controller Test Suite', () => {
+  beforeEach(() => {
+    const { request, response } = setupReqRes();
+    req = request;
+    res = response;
+  });
+
+  afterEach(() => {
+    clearMocks();
+  });
+
+  test('should validate render500ErrorHelper is called', async () => {
+    findOneLessonService.mockReturnValueOnce(new Error());
+
+    await renderLessonDetailsController(req, res);
+
+    validateMockValueToHaveBeenCalled(render500ErrorHelper);
+  });
+
+  test('should validate redirectNonexistentCourseHelper is called', async () => {
+    findOneLessonService.mockReturnValueOnce(null);
+
+    await renderLessonDetailsController(req, res);
+
+    validateMockValueToHaveBeenCalled(redirectNonexistentCourseHelper);
+  });
+
+  test('should validate res.status & res.render is called', async () => {
+    findOneLessonService.mockReturnValueOnce({
+      students: expect.anything(),
+      instructors: expect.anything(),
+    });
+
+    await renderLessonDetailsController(req, res);
+
+    const { status, render } = res;
+
+    validateMockValueToHaveBeenCalled(status);
+    validateMockValueToHaveBeenCalled(render);
+  });
+});
--- api/controllers/lessonsControllers/renderLessonDetails.controller.js
+++ api/controllers/lessonsControllers/renderLessonDetails.controller.js
@@ -1,4 +1,27 @@
-exports.renderLessonDetailsController = (req, res) => {
+const {
+  findOneLessonService,
+} = require('../../../database/services/modelServices/lessonServices');
+const {
+  render500ErrorHelper,
+  redirectNonexistentCourseHelper,
+} = require('../helpers');
+
+exports.renderLessonDetailsController = async (req, res) => {
   const { id } = req.params;
-  console.log(req.params);
+  const { local } = req.user;
+
+  const isLesson = await findOneLessonService(id);
+
+  if (isLesson instanceof Error) {
+    render500ErrorHelper(res);
+    return;
+  }
+
+  // TODO: refactor redirectNonexistent... to take care of both lessons and courses.
+  if (isLesson === null) {
+    redirectNonexistentCourseHelper(req, res);
+    return;
+  }
+
+  res.status(200).render('users/common/lesson-details', { local, isLesson });
 };
